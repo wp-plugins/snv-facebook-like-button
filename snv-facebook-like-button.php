4962aa7ca5f46f61e83386ac17035b66ac154cb6
studionashvegas <studionashvegas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-24 23:05:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/snv-facebook-like-button/trunk@233258 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- snv-facebook-like-button.php
+++ snv-facebook-like-button.php
@@ -10,6 +10,7 @@ Requires at least: 2.3
 Plugin URI: http://www.studionashvegas.com/plugins/facebook-like-button-plugin/
 Tested up to: 2.9.2
 Stable tag: 1.0
+Version: 1.0 
 
 This plugin allows you to drop in a very simple widget that showcases the new Facebook "like" button powered by OpenGraph
 */
